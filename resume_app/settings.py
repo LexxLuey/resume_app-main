5886c90ac33865f8c4df3b45708c6289bb4b585e
LexxLuey <biggestluey@gmail.com>
2021-12-16 13:03:17 +0100

    Update settings.py

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -147,10 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
