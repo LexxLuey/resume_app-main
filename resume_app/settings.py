4e2824e17dbc1a3ab6a255dfa68d9d9fcab569b3
LexxLuey <biggestluey@gmail.com>
2021-12-16 10:04:40 +0100

    static updates

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -129,10 +129,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     os.path.join(BASE_DIR, 'media')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
